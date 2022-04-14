96005bd94ae6da3b7ac9b691e9711344df14149f
Abdul Karim Gizzini <78584545+abdulkarimgizzini@users.noreply.github.com>
2022-04-14 14:30:28 +0200

    Add files via upload

---
+++ Matlab/LSTM_MLP_RP.m
@@ -0,0 +1,58 @@
+clc;clearvars;close all; warning('off','all');
+mobility  = 'H';
+modu = 'QPSK';
+load(['./',mobility,'/',modu,'/Testing_Simulation_variables.mat']);
+scheme                    = 'DPA';
+DL                        = 'LSTM_DNN';
+nBitPerSym                = 2;
+M                         = 2 ^ nBitPerSym;
+Pow                       = mean(abs(qammod(0:(M-1),M)).^2); 
+Testing_Data_set_size     = 2000;
+SNR_p                     = (0:5:40)';
+EbN0dB                    = SNR_p;
+nSym                      = 50;
+Interleaver_Columns       = (nBitPerSym * 48 * nSym) / 16;
+N_SNR                      = size(SNR_p,1);
+Phf                        = zeros(N_SNR,1);
+Err_LSTM  = zeros(N_SNR,1);
+Ber_LSTM  = zeros(N_SNR,1);
+dpositions             = [1:6, 8:20, 22:31, 33:45, 47:52].'; 
+for i = 1:size(SNR_p,1) 
+      disp(['Running Simulation, SNR = ', num2str(EbN0dB(i))]);
+     tic;
+     
+     % Load Testing Simulation Parameters (Tx_bits, received frames, true channels, etc...)
+     load(['./',mobility,'/',modu,'/Testing_Simulation_',num2str(EbN0dB(i)),'.mat']);
+     
+     
+     % Load LSTM Results
+     load(['./',mobility,'/',modu,'/', scheme,'_',DL,'_Results_' num2str(i),'.mat']);
+
+     TestY = eval([scheme,'_test_y_',num2str(i)]);
+     TestY = permute(TestY,[3 2 1]);
+     TestY = TestY(1:48,:,:) + 1i*TestY(49:96,:,:);
+     
+     PredictionY = eval([scheme,'_corrected_y_',num2str(i)]);
+     PredictionY = permute(PredictionY,[3 2 1]);
+
+  
+   
+
+    for u = 1:Testing_Data_set_size
+          t = True_Channels_Structure(dpositions,2:end,u);
+          t1 = TestY(:,:,u);
+
+         H_LSTM = PredictionY(:,:,u);
+ 
+        Phf(i) = Phf(i) + mean(sum(abs(True_Channels_Structure(dpositions,2:end,u)).^2)); 
+        Err_LSTM (i) =  Err_LSTM (i) +  mean(sum(abs(H_LSTM - True_Channels_Structure(dpositions,2:end,u)).^2)); 
+        
+        % IEEE 802.11p Rx
+        Bits_LSTM     = de2bi((qamdemod(sqrt(Pow) * (Received_Symbols_FFT_Structure(dpositions ,:,u) ./ H_LSTM),M)));
+        Ber_LSTM(i)   = Ber_LSTM(i) + biterr(wlanScramble(vitdec((matintrlv((deintrlv(Bits_LSTM(:),Random_permutation_Vector)).',Interleaver_Columns,16).'),poly2trellis(7,[171 133]),34,'trunc','hard'),93),TX_Bits_Stream_Structure(:,u));
+    end
+   toc;
+end
+Phf = Phf ./ Testing_Data_set_size;
+ERR_LSTM = Err_LSTM ./ (Testing_Data_set_size * Phf); 
+BER_LSTM = Ber_LSTM/ (Testing_Data_set_size * nSym * 48 * nBitPerSym);
