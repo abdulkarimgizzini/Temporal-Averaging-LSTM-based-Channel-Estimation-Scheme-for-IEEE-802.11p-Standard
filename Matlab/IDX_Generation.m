9290008cb8f91182b4b94e5385ae7240bf0c407d
Abdul Karim Gizzini <78584545+abdulkarimgizzini@users.noreply.github.com>
2022-09-03 11:08:40 +0200

    Delete Matlab directory

--- Matlab/IDX_Generation.m
+++
@@ -1,8 +0,0 @@
-clc;clear all; close all;
-IDX = 6000;
-TR  = 0.83;
-TE  = 0.17;
-All_IDX = 1:IDX;
-Tr_IDX = randperm(IDX,TR*IDX); 
-Te_IDX = setdiff(All_IDX,Tr_IDX);
-save(['./IDX',num2str(IDX),'.mat'],  'Tr_IDX','Te_IDX');
